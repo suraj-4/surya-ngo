7e6522cdafc1946c220e490d7fe332e96b016b0b
surya <105719685+suraj-4@users.noreply.github.com>
2023-12-10 17:44:28 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -230,7 +230,7 @@ const createChatLi = (message, className) => {
     // Create a chat <li> element with passed message and className
     const chatLi = document.createElement("li");
     chatLi.classList.add("chat", ${className});
-    let chatContent = className === "outgoing" ? <p></p> : <span class="material-symbols-outlined">smart_toy</span><p></p>;
+    let chatContent = className === "outgoing" ? <p></p> : <i class="fa-solid fa-headset"></i>><p></p>;
     chatLi.innerHTML = chatContent;
     chatLi.querySelector("p").textContent = message;
     return chatLi; // return chat <li> element
